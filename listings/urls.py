406b4d23888903b1b7d464341a62d98fbf0464af
Quinn Krug <quinn.krug@gmail.com>
2020-05-29 10:07:29 -0600

    set up routes for listing app

---
+++ listings/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+
+from . import views
+# if we leave the '' empty in the path method below, it will default to this app/model. So in this case it would be root_address + /listings
+urlpatterns = [
+    path('', views.index, name='listings'),
+    path('<int:listing_id>', views.listing, name='listing'),
+    path('<int:listing_id>', views.search, name='search')
+]
