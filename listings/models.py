9757eb3a99ccc1fbed340a3819b3a552dc16319c
Quinn Krug <quinn.krug@gmail.com>
2020-06-01 10:55:19 -0600

    create listing model

--- listings/models.py
+++ listings/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Listing(model.Model):
+    realtor = models.ForeignKey(Realtor, on_delete=models.DO_NOTHING)
+    title = models.CharField(max_length=200)
+    address = models.CharField(max_length=200)
+    city = models.CharField(max_length=200)
+    state = models.CharField(max_length=100)
+    zipcode = models.CharField(max_length=20)
