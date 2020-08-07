fc7737264e633bc9b8373d288113e8869ed821fc
Quinn Krug <quinn.krug@gmail.com>
2020-08-07 09:38:28 -0600

    set up contact model

--- contacts/models.py
+++ contacts/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-class Contacts(model.Models)
+class Contacts(model.Models):
+    listing = models.CharField(max_length=200)
+    listing_id = models.IntegerField()
+    name = models.CharField(max_length=200)
+    email = models.CharField(max_length=100)
+    phone = models.CharField(max_length=100)
+    message = models.TextField(blank=True)
+    # blank=True makes this opitinal
+    contact_date = models.DateTimeField(default=datetime.now, blank=True)
+    user_id = models.IntegerField(blank=True)
+    def __str__(self):
+    return self.name
